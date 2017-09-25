0016cebd867fd3b8093397b2c96066bed3dea17d
Red-Portal <msca8h@naver.com>
2017-09-25 18:06:34 +0900

    fixed test link error, suppressing unused variable warning

--- test/cuda_exception_test.cpp
+++ test/cuda_exception_test.cpp
@@ -8,12 +8,11 @@ TEST(mgcpp_exception, mgcpp_error_check)
 {
     using mgcpp::internal::cuda_mem_get_info;
 
-    float* ptr = nullptr;
-
     size_t free_memory= 0;
     cuda_mem_get_info(&free_memory, nullptr);
 
     EXPECT_DEATH({
+            float* ptr = nullptr;
             mgcpp_error_check(
                 ptr = mgcpp::cuda_malloc<float>(free_memory * 2));
         }, "death by mgcpp_error_check");
