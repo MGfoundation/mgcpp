2d2036a69298d9ccb56ff07eede8ddfa2d8a5f7d
Red-Portal <msca8h@naver.com>
2017-09-25 17:13:03 +0900

    reformatted exception header

--- include/mgcpp/cuda/exception.hpp
+++ include/mgcpp/cuda/exception.hpp
@@ -18,35 +18,41 @@
 #endif
 
 #ifndef MGCPP_THROW_BAD_ALLOC
-#define MGCPP_THROW_BAD_ALLOC MGCPP_THROW(std::bad_alloc())
+#define MGCPP_THROW_BAD_ALLOC                   \
+    MGCPP_THROW(std::bad_alloc())
 #endif
 
 #ifndef MGCPP_THROW_LENGTH_ERROR
-#define MGCPP_THROW_LENGTH_ERROR(MESSAGE) MGCPP_THROW(std::length_error(MESSAGE))
+#define MGCPP_THROW_LENGTH_ERROR(MESSAGE)       \
+    MGCPP_THROW(std::length_error(MESSAGE))
 #endif
 
 #ifndef MGCPP_THROW_OUT_OF_RANGE
-#define MGCPP_THROW_OUT_OF_RANGE(MESSAGE) MGCPP_THROW(std::out_of_range(MESSAGE))
+#define MGCPP_THROW_OUT_OF_RANGE(MESSAGE)       \
+    MGCPP_THROW(std::out_of_range(MESSAGE))
 #endif
 
 #ifndef MGCPP_THROW_RUNTIME_ERROR
-#define MGCPP_THROW_RUNTIME_ERROR(MESSAGE) MGCPP_THROW(std::runtime_error(MESSAGE))
+#define MGCPP_THROW_RUNTIME_ERROR(MESSAGE)      \
+    MGCPP_THROW(std::runtime_error(MESSAGE))
 #endif
 
 #ifndef MGCPP_THROW_OVERFLOW_ERROR
-#define MGCPP_THROW_OVERFLOW_ERROR(MESSAGE) MGCPP_THROW(std::overflow_error(MESSAGE))
+#define MGCPP_THROW_OVERFLOW_ERROR(MESSAGE)     \
+    MGCPP_THROW(std::overflow_error(MESSAGE))
 #endif
 
 #ifndef MGCPP_THROW_RUNTIME_ERROR
-#define MGCPP_THROW_RUNTIME_ERROR(MESSAGE) MGCPP_THROW(std::underflow_error(MESSAGE))
+#define MGCPP_THROW_RUNTIME_ERROR(MESSAGE)      \
+    MGCPP_THROW(std::underflow_error(MESSAGE))
 #endif
 
 #ifndef mgcpp_error_check
-#define mgcpp_error_check(EXP)                                  \
-    do{try{EXP;}catch(std::exception const& e){                 \
-            MGCPP_ERROR_MESSAGE_HANDLER(                        \
-                "[mgcpp errror]\nerror: %s\nposition: %s %d\n", \
-                e.what(), __FILE__, __LINE__);                  \
+#define mgcpp_error_check(EXP)                  \
+    do{try{EXP;}catch(std::exception const& e){ \
+            MGCPP_ERROR_MESSAGE_HANDLER(        \
+                "[mgcpp errror]\n%s\n%s %d\n",  \
+                e.what(), __FILE__, __LINE__);  \
             if(MGCPP_ABORT_ON_ERROR) exit(1);}}while(false)
 #endif
 
