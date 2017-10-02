181e8cc36c7231a5a71391b0c031883ee89fe9a6
Red-Portal <msca8h@naver.com>
2017-10-02 19:39:36 +0900

    added test file for cpu::matrix. added file to build system

---
+++ test/cpu_matrix.cpp
@@ -0,0 +1,11 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <gtest/gtest.h>
+
+#include <mgcpp/cpu_matrix.hpp>
+
+
