7b28373ac20bc935a717b8e4b98555b056662b00
Red-Portal <msca8h@naver.com>
2017-10-02 19:45:06 +0900

    added init_from_cpu_matrix test

--- test/gpu_matrix_test.cpp
+++ test/gpu_matrix_test.cpp
@@ -9,6 +9,7 @@
 #include <gtest/gtest.h>
 
 #include <mgcpp/cuda/memory.hpp>
+#include <mgcpp/cpu_matrix.hpp>
 #define private public
 #include <mgcpp/gpu_matrix.hpp>
 
@@ -63,7 +64,7 @@ TEST(gpu_matrix, contextless_dimension_initializing_constructor)
     {
         size_t row_dim = 10;
         size_t col_dim = 10;
-        float init_val = 10;
+        float init_val = 7;
         mgcpp::gpu::matrix<float> mat(row_dim, col_dim, init_val);
 
         auto after = mgcpp::cuda_mem_get_info();
@@ -82,7 +83,6 @@ TEST(gpu_matrix, contextless_dimension_initializing_constructor)
         {
             for(size_t j = 0; j < col_dim; ++j)
             {
-
                 EXPECT_EQ(mat.check_value(i, j), init_val)
                     << "index i: " << i << " j: " << j;
             }
@@ -183,6 +183,46 @@ TEST(gpu_matrix, matrix_resize_init)
         auto fivebyfive_freemem = fivebyfive.value().first;
 
         EXPECT_GT(twobytwo_freemem, fivebyfive_freemem);
+    }
+
+    auto finally = mgcpp::cuda_mem_get_info();
+    EXPECT_TRUE(finally);
+    auto final_freemem = initial.value().first;
+    EXPECT_EQ(initial_freemem, final_freemem);
+}
+
+TEST(gpu_matrix, init_from_cpu_matrix)
+{
+    size_t row_dim = 5;
+    size_t col_dim = 5;
+    size_t init_val = 17;
+
+    mgcpp::cpu::matrix<float> cpu_mat(row_dim, col_dim, init_val);
+
+    for(size_t i = 0; i < row_dim; ++i)
+    {
+        for(size_t j = 0; j < col_dim; ++j)
+        {
+            EXPECT_EQ(cpu_mat(i, j), init_val)
+                << "index i: " << i << " j: " << j;
+        }
+    }
+
+    auto initial = mgcpp::cuda_mem_get_info();
+    EXPECT_TRUE(initial);
+
+    auto initial_freemem = initial.value().first;
+    {
+        mgcpp::gpu::matrix<float> gpu_mat(cpu_mat);
+
+        for(size_t i = 0; i < row_dim; ++i)
+        {
+            for(size_t j = 0; j < col_dim; ++j)
+            {
+                EXPECT_EQ(gpu_mat.check_value(i, j), init_val)
+                    << "index i: " << i << " j: " << j;
+            }
+        }
     }
 
     auto finally = mgcpp::cuda_mem_get_info();
