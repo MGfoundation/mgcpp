d85ed052de4a63cd79ad82312b582e8bc6592f43
Red-Portal <msca8h@naver.com>
2017-10-03 17:47:39 +0900

    debug commit

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -4,6 +4,8 @@
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <cstdio>
+
 #include <gtest/gtest.h>
 
 #include <mgcpp/gpu_matrix.hpp>
@@ -19,11 +21,15 @@ TEST(operation_mult, row_major_multiplication)
     mgcpp::gpu::matrix<float> A_mat(context, 2, 4);
     A_mat.copy_from_host(A_init_mat);
 
+    printf("safe\n");
+
     mgcpp::gpu::matrix<float> B_mat(context, 4, 2);
     B_mat.copy_from_host(B_init_mat);
 
+    printf("safe\n");
     auto C_mat = mgcpp::mult(A_mat, B_mat);
 
+    printf("safe\n");
     EXPECT_EQ(C_mat.rows(), 2);
     EXPECT_EQ(C_mat.columns(), 2);
 
