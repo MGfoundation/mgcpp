046b7afd69cb3c576531dec488214cb8d7d16e17
Red-Portal <msca8h@naver.com>
2017-10-03 17:13:08 +0900

    added operation_mult_test to build system

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -4,3 +4,10 @@
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <gtest/gtest.h>
+
+#include <mgcpp/gpu_matrix.hpp>
+#include <mgcpp/operation/mult.hpp>
+
+
+
