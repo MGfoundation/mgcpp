0ec42203809d4188102331245176b70cd2d5e3a1
Red-Portal <msca8h@naver.com>
2017-10-03 17:10:57 +0900

    added row major matxmat multiplication

--- include/mgcpp/operations/mult.hpp
+++ include/mgcpp/operations/mult.hpp
@@ -11,35 +11,41 @@
 
 namespace mgcpp
 {
-    template<typename ElemType,
-             size_t DeviceId,
-             storage_order StoreOrder>
+    template<typename T,
+             size_t Device,
+             storage_order SO>
     gpu::matrix
-    mult(gpu::matrix<ElemType, DeviceId, StoreOrder> const& first,
-         gpu::matrix<ElemType, DeviceId, StoreOrder> const& second);
+    mult(gpu::matrix<T, Device, SO> const& first,
+         gpu::matrix<T, Device, SO> const& second);
 
-    template<typename ElemType,
-             size_t IdFirst, size_t IdSecond,
-             storange_order StoreOrder>
-    gpu::matrix
-    mult(gpu::matrix<ElemType, IdFirst, StoreOrder> const& first,
-         gpu::matrix<ElemType, IdSecond, StoreOrder> const& second);
+    // template<typename T,
+    //          size_t Device>
+    // gpu::matrix
+    // mult(gpu::matrix<T, Device, storage_order::column_major> const& first,
+    //      gpu::matrix<T, Device, storage_order::column_major> const& second);
 
-    template<typename ElemType,
-             size_t DeviceId,
+    // template<typename T,
+    //          size_t IdFirst, size_t IdSecond,
+    //          storange_order StoreOrder>
+    // gpu::matrix
+    // mult(gpu::matrix<T, IdFirst, StoreOrder> const& first,
+    //      gpu::matrix<T, IdSecond, StoreOrder> const& second);
+
+    template<typename T,
+             size_t Device,
              storage_order StorOrder>
     void
     mult_assign(
-        gpu::matrix<ElemType, DeviceId, StoreOrder>& first,
-        gpu::matrix<ElemType, DeviceId, StoreOrder> const& second);
+        gpu::matrix<T, Device, StoreOrder>& first,
+        gpu::matrix<T, Device, StoreOrder> const& second);
 
-    template<typename ElemType,
-             size_t IdFirst, size_t IdSecond,
-             storage_order StoreOrder>
-    void
-    mult_assign(
-        gpu::matrix<ElemType, IdFirst, StoreOrder>& first,
-        gpu::matrix<ElemType, IdSecond, StoreOrder> const& second);
+    // template<typename T,
+    //          size_t IdFirst, size_t IdSecond,
+    //          storage_order StoreOrder>
+    // void
+    // mult_assign(
+    //     gpu::matrix<T, IdFirst, StoreOrder>& first,
+    //     gpu::matrix<T, IdSecond, StoreOrder> const& second);
 }
 
 #include <mgcpp/operations/mult.tpp>
--- include/mgcpp/operations/mult.tpp
+++ include/mgcpp/operations/mult.tpp
@@ -5,13 +5,42 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <mgcpp/operations/mult.hpp>
+#include <mgcpp/system/error_code.hpp>
+#include <mgcpp/system/exception.hpp>
+
+#include <cublas_v2.h>
 
 namespace mgcpp
 {
-    template<typename ElemType,
-             size_t DeviceId,
-             storage_order StoreOrder>
+    template<size_t Device>
     gpu::matrix
-    mult(gpu::matrix<ElemType, DeviceId, StoreOrder> const& first,
-         gpu::matrix<ElemType, DeviceId, StoreOrder> const& second);
+    mult(gpu::matrix<float, Device, row_major> const& first,
+         gpu::matrix<float, Device, row_major> const& second)
+    {
+        float const alpha = 1;
+        float const beta = 0;
+
+        size_t m = second.rows(); // this is right to be flipped
+        size_t k = second.columns();
+        size_t n = first.columns();
+
+        gpu::matrix<float, Device, row_major> result{};
+
+        thread_context* context =  first.get_thread_context();
+        
+        std::error_code status =
+            cublasSgemm_v2(context->get_cublas(Device),
+                           CUBLAS_OP_N, CUBLAS_OP_N,
+                           m, n, k,
+                           &alpha,
+                           second.get_data(), m,
+                           first.get_data(), k,
+                           &beta,
+                           result.get_data());
+
+        if(status != status_t::success)
+            MGCPP_THROW_SYSTEM_ERROR(status);
+
+        return result;
+    }
 }
