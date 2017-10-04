6a0cbf066264e2f39074acb19bc1aec1a5dec333
Red-Portal <msca8h@naver.com>
2017-10-04 17:54:40 +0900

    debug commit

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -16,12 +16,12 @@ TEST(operation_mult, row_major_multiplication)
     mgcpp::cpu::matrix<float> A_init_mat(2, 4, 2);
     mgcpp::cpu::matrix<float> B_init_mat(4, 2, 4);
 
-    printf("safe?\n");
     mgcpp::gpu::matrix<float> A_mat(2, 4);
     A_mat.copy_from_host(A_init_mat);
 
     printf("safe?\n");
     mgcpp::gpu::matrix<float> B_mat(4, 2);
+    printf("safe?\n");
     B_mat.copy_from_host(B_init_mat);
 
     printf("safe?\n");
