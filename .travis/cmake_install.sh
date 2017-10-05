72ed42ceb4ae3c0e2fedb542e75bfd66d4bc6be8
Red-Portal <msca8h@naver.com>
2017-10-05 20:43:06 +0900

    fixed travis script

--- .travis/cmake_install.sh
+++ .travis/cmake_install.sh
@@ -1,13 +1,15 @@
 travis_retry wget https://cmake.org/files/v3.9/cmake-3.9.4.tar.gz
 
-travis_retry sudo mv cmake-3.9.4.tar.gz ~
-travis_retry cd ~
-travis_retry tar -xzf cmake-3.9.4.tar.gz 
-travis_retry cd cmake-3.9.4/ 
+sudo mv cmake-3.9.4.tar.gz ~
+cd ~
+tar -xzf cmake-3.9.4.tar.gz 
+cd cmake-3.9.4/ 
 
-travis_retry ./bootstrap
+./bootstrap
 
-travis_retry make
-travis_retry make install
+make -j2 -s 
+make install
 
 CMAKE_VER="3.9.4"
+
+cd ~/Red-Portal/mgcpp
--- .travis/cuda_install.sh
+++ .travis/cuda_install.sh
@@ -1,5 +1,7 @@
 travis_retry wget -P ./ https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb
 
+cd cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb
+
 sudo dpkg -i cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda
