6f4f8b0efb74a6c7088bf336f9514074f02eeb69
Red-Portal <msca8h@naver.com>
2017-10-05 20:47:50 +0900

    moved gtest install script to a seperate file

---
+++ .travis/gtest_install.sh
@@ -0,0 +1,10 @@
+travis_retry git clone https://github.com/google/googletest.git
+
+cd googletest
+
+~/cmake-${CMAKE_VER}/bin/cmake -G "Unix Makefiles"
+
+make -j2 -s
+sudo make install
+
+cd ~/Red-Portal/mgcpp
