9280c7a10aa55e49429b9f2c0718c5d60db508cf
Red-Portal <msca8h@naver.com>
2017-10-05 17:44:39 +0900

    updated travis script

--- .travis/cmake_install.sh
+++ .travis/cmake_install.sh
@@ -1,7 +1,7 @@
 travis_retry wget https://cmake.org/files/v3.9/cmake-3.9.4.tar.gz
 travis_retry sudo mv cmake-3.9.4.tar.gz ~
 travis_retry cd ~
-travis_retry tar -xvzf cmake-3.9.4.tar.gz 
+travis_retry tar -xzf cmake-3.9.4.tar.gz 
 travis_retry cd cmake-3.9.4/ 
 
 travis_retry ./bootstrap
