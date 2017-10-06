5718dafc22dd05cc77979609fd11635d092cb3df
Red-Portal <msca8h@naver.com>
2017-10-06 17:58:18 +0900

    debug commit

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -20,8 +20,11 @@ TEST(operation_mult, row_major_multiplication)
     mgcpp::cpu::matrix<float> B_init_mat(4, 2, 4);
 
 
+    std::cout << "good" << std::endl;
     mgcpp::gpu::matrix<float> A_mat(A_init_mat);
+    std::cout << "good" << std::endl;
     mgcpp::gpu::matrix<float> B_mat(B_init_mat);
+    std::cout << "good" << std::endl;
 
     // mgcpp::gpu::matrix<float> A_mat(2, 4);
     // A_mat.copy_from_host(A_init_mat);
