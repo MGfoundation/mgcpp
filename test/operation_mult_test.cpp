daba49768ae7fd83114966d5a41449085d54baa8
Red-Portal <msca8h@naver.com>
2017-10-06 18:00:04 +0900

    reformatted mult_test but build still failing

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -19,17 +19,8 @@ TEST(operation_mult, row_major_multiplication)
     mgcpp::cpu::matrix<float> A_init_mat(2, 4, 2);
     mgcpp::cpu::matrix<float> B_init_mat(4, 3, 4);
 
-    std::cout << "good" << std::endl;
     mgcpp::gpu::matrix<float> A_mat(A_init_mat);
-    std::cout << "good" << std::endl;
     mgcpp::gpu::matrix<float> B_mat(B_init_mat);
-    std::cout << "good" << std::endl;
-
-    // mgcpp::gpu::matrix<float> A_mat(2, 4);
-    // A_mat.copy_from_host(A_init_mat);
-
-    // mgcpp::gpu::matrix<float> B_mat(4, 2);
-    // B_mat.copy_from_host(B_init_mat);
 
     auto C_mat = mgcpp::mult(A_mat, B_mat);
 
