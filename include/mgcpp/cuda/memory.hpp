b24d6cb202dd78234c2ce9e8890afdcb27920999
Red-Portal <msca8h@naver.com>
2017-10-07 21:08:15 +0900

    minor reformats

--- include/mgcpp/cuda/memory.hpp
+++ include/mgcpp/cuda/memory.hpp
@@ -1,10 +1,11 @@
+
 //          Copyright RedPortal 2017 - 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef MGCPP_CUDA_MEMORY_HPP
-#define MGCPP_CUDA_MEMORY_HPP
+#ifndef _MGCPP_CUDA_MEMORY_HPP_
+#define _MGCPP_CUDA_MEMORY_HPP_
 
 #include <outcome.hpp>
 
