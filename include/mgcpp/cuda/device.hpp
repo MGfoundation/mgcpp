78419b4feee7e1bb7c26a03cdb93595dc36b2457
Red-Portal <msca8h@naver.com>
2017-10-07 21:38:13 +0900

    added set_device wrapper function
    maybe give some improvements? we'll see how I can exploit C++ for this
    one. (operator dot overloading will be a great toy to have)

---
+++ include/mgcpp/cuda/device.hpp
@@ -0,0 +1,23 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _MGCPP_CUDA_DEVICE_HPP_
+#define _MGCPP_CUDA_DEVICE_HPP_
+
+#include <outcome.hpp>
+
+#include <mgcpp/system/error_code.hpp>
+
+#include <cstdlib>
+
+namespace mgcpp
+{
+    outcome::result<void>
+    cuda_set_device(size_t device_id) noexcept;
+}
+
+#include <mgcpp/cuda/device.tpp>
+#endif
