c650213494e193732447167479c9d30bff99e1ce
Red-Portal <msca8h@naver.com>
2017-10-09 17:09:32 +0900

    debug commit

--- test/operation_mult_test.cpp
+++ test/operation_mult_test.cpp
@@ -22,20 +22,20 @@ TEST(operation_mult, row_major_multiplication)
     mgcpp::gpu::matrix<float> A_mat(A_init_mat);
     mgcpp::gpu::matrix<float> B_mat(B_init_mat);
 
-    // auto C_mat = mgcpp::mult(A_mat, B_mat);
+    auto C_mat = mgcpp::mult(A_mat, B_mat);
 
-    // auto C_mat_host = C_mat.copy_to_host();
+    auto C_mat_host = C_mat.copy_to_host();
     
-    // auto shape = C_mat_host.shape();
-    // EXPECT_EQ(shape.first, 2);
-    // EXPECT_EQ(shape.second, 3);
+    auto shape = C_mat_host.shape();
+    EXPECT_EQ(shape.first, 2);
+    EXPECT_EQ(shape.second, 3);
 
-    // for(size_t i = 0; i < shape.first; ++i)
-    // {
-    //     for(size_t j = 0; j < shape.second; ++j)
-    //     {
-    //         EXPECT_EQ(C_mat_host(i, j), 32)
-    //             << "i: " << i << " j: " << j; 
-    //     } 
-    // }
+    for(size_t i = 0; i < shape.first; ++i)
+    {
+        for(size_t j = 0; j < shape.second; ++j)
+        {
+            EXPECT_EQ(C_mat_host(i, j), 32)
+                << "i: " << i << " j: " << j; 
+        } 
+    }
 }
