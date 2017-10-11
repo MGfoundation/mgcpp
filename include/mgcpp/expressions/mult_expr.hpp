7a29a3b27dfc3b5882779d4efedb9185bec58902
Red-Portal <msca8h@naver.com>
2017-10-11 17:52:18 +0900

    added header guard to mult_expr.hpp

--- include/mgcpp/expressions/mult_expr.hpp
+++ include/mgcpp/expressions/mult_expr.hpp
@@ -4,17 +4,23 @@
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#ifndef _MGCPP_EXPRESSIONS_MULT_EXPR_HPP_
+#define _MGCPP_EXPRESSIONS_MULT_EXPR_HPP_
+
 namespace mgcpp
 {
-    typename <typename LhsExpr, typename RhsExpr>
+    template<typename LhsExpr, typename RhsExpr>
     struct mult_expr
     {
-        RhsExpr& lhs; 
-        LhsExpr& rhs;
+        RhsExpr&& lhs; 
+        LhsExpr&& rhs;
 
-        mult_expr(LhsExpr& lhs, RhsExpr& rhs);
+        mult_expr(LhsExpr&& lhs, RhsExpr&& rhs);
     };
 
-    inline mult_expr<typename LhsExpr, typename RhsExpr>
-    operator*(LhsExpr& lhs, RhsExpr& rhs);
+    template<typename LhsExpr, typename RhsExpr>
+    inline mult_expr<LhsExpr, RhsExpr>
+    operator*(LhsExpr&& lhs, RhsExpr&& rhs);
 }
+
+#endif
