1c389ab46410d338aace88b1e3575c2fe47e6ddf
Red-Portal <msca8h@naver.com>
2017-10-19 21:04:10 +0900

    added declaration for eval

---
+++ include/mgcpp/expressions/expr_eval.hpp
@@ -0,0 +1,17 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _MGCPP_EXPRESSIONS_EXPR_EVAL_HPP_
+#define _MGCPP_EXPRESSIONS_EXPR_EVAL_HPP_
+
+namespace mgcpp
+{
+    template<typename... Args>
+    void
+    eval();
+}
+
+#endif
