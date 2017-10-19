6a3fe91febcf48140a33a8f5bc58736c8a33b636
Red-Portal <msca8h@naver.com>
2017-10-19 20:38:55 +0900

    added void_t type(which is a VS2017 construct)

---
+++ include/mgcpp/type_traits/type_traits.hpp
@@ -0,0 +1,16 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _MGCPP_TYPE_TRAITS_TYPE_TRAITS_HPP_
+#define _MGCPP_TYPE_TRAITS_TYPE_TRAITS_HPP_
+
+namespace mgcpp
+{
+    template<typename... Args>
+    using void_t = void;
+}
+
+#endif
