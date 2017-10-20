f192487ff26f00db36e33e91e8cd69a2f23cb24b
Red-Portal <msca8h@naver.com>
2017-10-21 02:44:07 +0900

    added vector to cpu/forward.hpp

--- include/mgcpp/cpu/forward.hpp
+++ include/mgcpp/cpu/forward.hpp
@@ -8,6 +8,7 @@
 #define _MGCPP_CPU_FORWARD_HPP_
 
 #include <mgcpp/global/storage_order.hpp>
+#include <mgcpp/global/allignment.hpp>
 
 namespace mgcpp
 {
@@ -16,6 +17,10 @@ namespace mgcpp
         template<typename ElemType,
                  storage_order StoreOrder = row_major>
         class matrix;
+
+        template<typename ElemType,
+                 allignment Allign = row>
+        class vector;
     }
 }
 
