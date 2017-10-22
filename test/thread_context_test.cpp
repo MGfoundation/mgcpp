a81157a3dc990ffb7ebefc7f208acd8bbcb68ad8
Red-Portal <msca8h@naver.com>
2017-10-23 01:29:45 +0900

    added short form license, added multi-gpu policy condition

--- test/thread_context_test.cpp
+++ test/thread_context_test.cpp
@@ -1,7 +1,13 @@
 
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <thread>
 
 #include <mgcpp/context/global_context.hpp>
+#include "test_policy.hpp"
 
 #include <gtest/gtest.h>
 
@@ -19,7 +25,9 @@ TEST(thead_context, request_cublas_handle)
 
 TEST(thead_context, request_cublas_handle_different_device)
 {
-    auto* context = &mgcpp::global_context::get_thread_context();
+    if(mgcpp::test_policy::get_policy().device_num() >= 2)
+    {
+        auto* context = &mgcpp::global_context::get_thread_context();
 
     auto cublas_context_one = context->get_cublas_context(0);
     auto cublas_context_two = context->get_cublas_context(1);
@@ -27,4 +35,5 @@ TEST(thead_context, request_cublas_handle_different_device)
     EXPECT_NE(cublas_context_one, cublas_context_two);
 
     mgcpp::global_context::reference_cnt_decr();
+    }
 }
