599a63519a5bcacdbdb965c69667569f008169aa
Red-Portal <msca8h@naver.com>
2017-10-24 20:03:01 +0900

    added signature for vector add operation

---
+++ include/mgcpp/operations/sum.hpp
@@ -0,0 +1,25 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _MGCPP_OPERATIONS_SUM_HPP_
+#define _MGCPP_OPERATIONS_SUM_HPP_
+
+#include <mgcpp/gpu/forward.hpp>
+
+namespace mgcpp
+{
+    namespace strict
+    {
+        template<typename T,
+                 size_t DeviceId,
+                 allignment Allign>
+        inline T
+        sum(gpu::vector<T, DeviceId, Allign> const& vec);
+    }
+}
+
+#include <mgcpp/operations/sum.tpp>
+#endif
