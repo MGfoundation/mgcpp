afd4d31f22218d6aaa42cf965d3f1f72c96cb631
Red-Portal <msca8h@naver.com>
2017-10-26 20:53:39 +0900

    updated public interface headers

--- include/mgcpp/mgcpp.hpp
+++ include/mgcpp/mgcpp.hpp
@@ -4,13 +4,10 @@
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef _MGCPP_MGCPP_
-#define _MGCPP_MGCPP_
+#ifndef _MGCPP_MGCPP_HPP_
+#define _MGCPP_MGCPP_HPP_
 
-#include <mgcpp/cpu/matrix.hpp>
-#include <mgcpp/cuda/stdlib.hpp>
-#include <mgcpp/gpu/matrix.hpp>
-#include <mgcpp/gpu/vector.hpp>
-#include <mgcpp/strict/multiplication.hpp>
+#include <mgcpp/device/matrix.hpp>
+#include <mgcpp/device/vector.hpp>
 
 #endif
