efe656e5f3e1ddca40faf8d19ed6317eba6c6af7
Red-Portal <msca8h@naver.com>
2017-10-28 12:32:28 +0900

    added example custom cuda kernels

---
+++ include/mgcpp/kernels/fill.tpp
@@ -0,0 +1,23 @@
+
+//          Copyright RedPortal 2017 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mgcpp/kernels/fill.hpp>
+
+namespace mgcpp
+{
+    outcome::result<void>
+    fill(float* device_ptr, float value, size_t count)
+    {
+
+    }
+
+    outcome::result<void>
+    fill(double* device_ptr, double value, size_t count)
+    {
+        
+    }
+}
+
---
+++ src/kernels/fill.cu
@@ -0,0 +1,58 @@
+#include <mgcpp/kernels/fill.hpp>
+
+namespace mgcpp
+{
+    void fill64(void* ptr, uint64_t bitvec, size_t size)
+    {
+	
+    }
+
+    void fill32(void* ptr, uint32_t bitvec, size_t size)
+    {
+	
+    }
+
+    void fill16(void* ptr, uint16_t bitvec, size_t size)
+    {
+	
+    }
+
+    __host__ __device__  void
+    fill64_impl(uint64_t* ptr, uint64_t bitvec, size_t count)
+    {
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for (size_t i = idx;
+	     i < count;
+	     i += gridDim.x * blockDim.x)
+	{
+	    ptr[i] = bitvec;
+	}
+    }
+
+    __host__ __device__  void
+    fill32_impl(uint32_t* ptr, uint32_t bitvec, size_t count)
+    {
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for (size_t i = idx;
+	     i < count;
+	     i += gridDim.x * blockDim.x)
+	{
+	    ptr[i] = bitvec;
+	}
+    }
+
+    __host__ __device__  void
+    fill16_impl(uint16_t* ptr, uint16_t bitvec, size_t count)
+    {
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for (size_t i = idx;
+	     i < count;
+	     i += gridDim.x * blockDim.x)
+	{
+	    ptr[i] = bitvec;
+	}
+    }
+}
