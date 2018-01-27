1650349b053e2325b5d89cc0fb304ff2e56b446b
Red-Portal <msca8h@naver.com>
2018-01-28 01:30:51 +0900

    added test for function style mult expression

--- test/blaslv3_expression_test.cpp
+++ test/blaslv3_expression_test.cpp
@@ -34,3 +34,30 @@ TEST(mult_expr, row_major_mat_mat_mult)
         } 
     }
 }
+
+
+TEST(mult_expr, row_major_mat_mat_mult_func)
+{
+    using matrix = mgcpp::device_matrix<float>;
+
+    matrix A_mat({2, 4}, 2);
+    matrix B_mat({4, 3}, 4);
+
+    auto mult_expr = mgcpp::mult(A_mat, B_mat);
+
+    matrix C_mat; 
+    EXPECT_NO_THROW({C_mat = mult_expr.eval();});
+    
+    auto shape = C_mat.shape();
+    EXPECT_EQ(shape[0], 2);
+    EXPECT_EQ(shape[1], 3);
+
+    for(size_t i = 0; i < shape[0]; ++i)
+    {
+        for(size_t j = 0; j < shape[1]; ++j)
+        {
+            EXPECT_EQ(C_mat.check_value(i, j), 32)
+                << "i: " << i << " j: " << j; 
+        } 
+    }
+}
