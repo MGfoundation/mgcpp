fa5f6758bb74043468e543206d315e2ff2b25566
Red-Portal <msca8h@naver.com>
2018-02-15 20:04:33 +0900

    updated dmat_trans expression

---
+++ include/mgcpp/expressions/dmat_trans_expr.hpp
@@ -0,0 +1,36 @@
+
+//          Copyright RedPortal, mujjingun 2017 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _MGCPP_EXPRESSIONS_MAT_TRANS_EXPR_HPP_
+#define _MGCPP_EXPRESSIONS_MAT_TRANS_EXPR_HPP_
+
+#include <mgcpp/expressions/dmat_expr.hpp>
+
+namespace mgcpp {
+    template <typename Expr>
+    struct dmat_trans_expr
+        :  dmat_expr<dmat_trans_expr<Expr>>
+    {
+        using expr_type = typename std::decay<Expr>::type;
+        using result_type = typename expr_type;
+
+        Expr const& _mat;
+
+        inline dmat_trans_expr(Expr&& mat) noexcept;
+
+        inline decltype(auto) eval();
+    };
+
+    template <typename Expr>
+    inline dmat_trans_expr<Expr> trans(
+        dmat_expr<Expr> const& dense_mat_expr) noexcept;
+
+    template <typename Expr>
+    inline dmat_trans_expr<Expr> operator!(
+        dmat_expr<Expr> const& dense_mat_expr) noexcept;
+}  // namespace mgcpp
+
+#endif
