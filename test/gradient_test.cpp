8dc699c8237e966036a64f45ec47eb4a98f1144d
Gun Park <mujjingun@gmail.com>
2018-05-20 16:45:19 +0900

    add more tests for gradient

--- test/gradient_test.cpp
+++ test/gradient_test.cpp
@@ -56,8 +56,8 @@ TEST(gradient_test, dmat_add) {
 TEST(gradient_test, dmat_mul) {
   mgcpp::placeholder_node<0, mgcpp::dmat_expr, mgcpp::device_matrix<double>> ph0;
   mgcpp::placeholder_node<1, mgcpp::dmat_expr, mgcpp::device_matrix<double>> ph1;
-  auto add = ph0 * ph1;
-  auto sum = reduce_sum(add);
+  auto mul = ph0 * ph1;
+  auto sum = reduce_sum(mul);
   auto grad = mgcpp::grad(sum, ph0);
 
   mgcpp::eval_context ctx;
@@ -75,7 +75,7 @@ TEST(gradient_test, dmat_mul) {
   double R = sum.eval(ctx);
   double Rp = sum.eval(ctx2);
   auto numeric_approx = (Rp - R) / epsilon;
-  EXPECT_EQ(result.shape(), mgcpp::make_shape(2, 4));
+  EXPECT_EQ(result.shape(), mgcpp::make_shape(4, 4));
   for (size_t i = 0; i < result.shape()[0]; ++i) {
       for (size_t j = 0; j < result.shape()[1]; ++j) {
           EXPECT_FLOAT_EQ(result.check_value(i, j), numeric_approx);
@@ -84,3 +84,32 @@ TEST(gradient_test, dmat_mul) {
 }
 
 
+TEST(gradient_test, dmat_mul_add) {
+  mgcpp::placeholder_node<0, mgcpp::dmat_expr, mgcpp::device_matrix<double>> ph0;
+  mgcpp::placeholder_node<1, mgcpp::dmat_expr, mgcpp::device_matrix<double>> ph1;
+  auto expr = ph0 * ph1 + ph0 * ph0;
+  auto sum = reduce_sum(expr);
+  auto grad = mgcpp::grad(sum, ph0);
+
+  mgcpp::eval_context ctx;
+  mgcpp::device_matrix<double> mat1({4, 4}, 3);
+  mgcpp::device_matrix<double> mat2({4, 4}, 2);
+  ctx.feed(ph0, mat1);
+  ctx.feed(ph1, mat2);
+  auto result = grad.eval(ctx);
+
+  mgcpp::eval_context ctx2;
+  double epsilon = 0.00001;
+  mat1.set_value(0, 0, 3 + epsilon);
+  ctx2.feed(ph0, mat1);
+  ctx2.feed(ph1, mat2);
+  double R = sum.eval(ctx);
+  double Rp = sum.eval(ctx2);
+  auto numeric_approx = (Rp - R) / epsilon;
+  EXPECT_EQ(result.shape(), mgcpp::make_shape(4, 4));
+  for (size_t i = 0; i < result.shape()[0]; ++i) {
+      for (size_t j = 0; j < result.shape()[1]; ++j) {
+          EXPECT_FLOAT_EQ(result.check_value(i, j), numeric_approx);
+      }
+  }
+}
